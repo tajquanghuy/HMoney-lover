4225f8acfa4c54be9c9698a7631ea57502bbd558
huytq <huytq1.it@tripi.vn>
2022-09-08 16:08:50 +0700

    add keystore

---
+++ app/src/main/java/com/pigllet/hmoneylover/ui/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.pigllet.hmoneylover.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.pigllet.hmoneylover.R
+import com.pigllet.hmoneylover.billings.activities.BuyCoinActivity
+import com.pigllet.hmoneylover.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMainBinding.inflate(inflater, container, false).apply {
+            binding = this
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnStart.setOnClickListener {
+            findNavController().navigate(R.id.action_to_homeFragment)
+        }
+        binding.btnBuy.setOnClickListener {
+            startActivity(Intent(activity, BuyCoinActivity::class.java))
+        }
+    }
+
+}
